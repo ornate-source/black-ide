b9fde3004c05009587fd1623b316aadd9b5d1870
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2026-07-20 12:24:16 +0600

    chore: update latest release assets to version 1.121.04828

--- releases.js
+++ releases.js
@@ -1,131 +1,116 @@
 // Static release assets data source to prevent GitHub API rate limit issues
 window.LATEST_RELEASE = {
-  "tag_name": "1.121.04611",
+  "tag_name": "1.121.04828",
   "assets": [
     {
-      "name": "black-ide-1.121.04611-el8.aarch64.rpm",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide-1.121.04611-el8.aarch64.rpm",
-      "size": 184170165
+      "name": "black-ide-1.121.04828-el8.aarch64.rpm",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide-1.121.04828-el8.aarch64.rpm",
+      "size": 184526500
     },
     {
-      "name": "black-ide-1.121.04611-el8.armv7hl.rpm",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide-1.121.04611-el8.armv7hl.rpm",
-      "size": 173518519
+      "name": "black-ide-1.121.04828-el8.armv7hl.rpm",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide-1.121.04828-el8.armv7hl.rpm",
+      "size": 173896184
     },
     {
-      "name": "black-ide-1.121.04611-el8.ppc64le.rpm",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide-1.121.04611-el8.ppc64le.rpm",
-      "size": 209310590
+      "name": "black-ide-1.121.04828-el8.ppc64le.rpm",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide-1.121.04828-el8.ppc64le.rpm",
+      "size": 209668983
     },
     {
-      "name": "black-ide-1.121.04611-el8.x86_64.rpm",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide-1.121.04611-el8.x86_64.rpm",
-      "size": 188149217
+      "name": "black-ide-1.121.04828-el8.x86_64.rpm",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide-1.121.04828-el8.x86_64.rpm",
+      "size": 188521599
     },
     {
-      "name": "black-ide_1.121.04611_amd64.deb",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide_1.121.04611_amd64.deb",
-      "size": 132416328
+      "name": "black-ide_1.121.04828_amd64.deb",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide_1.121.04828_amd64.deb",
+      "size": 132594200
     },
     {
-      "name": "black-ide_1.121.04611_arm64.deb",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide_1.121.04611_arm64.deb",
-      "size": 123646964
+      "name": "black-ide_1.121.04828_arm64.deb",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide_1.121.04828_arm64.deb",
+      "size": 123562348
     },
     {
-      "name": "black-ide_1.121.04611_armhf.deb",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide_1.121.04611_armhf.deb",
-      "size": 122144892
+      "name": "black-ide_1.121.04828_armhf.deb",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide_1.121.04828_armhf.deb",
+      "size": 122158336
     },
     {
-      "name": "black-ide_1.121.04611_ppc64el.deb",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black-ide_1.121.04611_ppc64el.deb",
-      "size": 136443864
+      "name": "black-ide_1.121.04828_ppc64el.deb",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/black-ide_1.121.04828_ppc64el.deb",
+      "size": 136169528
     },
     {
-      "name": "black.ide-cli-darwin-arm64-1.121.0.tar.gz",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/black.ide-cli-darwin-arm64-1.121.0.tar.gz",
-      "size": 7883460
+      "name": "Black.IDE-linux-arm64-1.121.04828.tar.gz",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-linux-arm64-1.121.04828.tar.gz",
+      "size": 182641218
     },
     {
-      "name": "Black.IDE-darwin-arm64-1.121.0.zip",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-darwin-arm64-1.121.0.zip",
-      "size": 275147653
+      "name": "Black.IDE-linux-armhf-1.121.04828.tar.gz",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-linux-armhf-1.121.04828.tar.gz",
+      "size": 171912186
     },
     {
-      "name": "Black.IDE-linux-arm64-1.121.04611.tar.gz",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-linux-arm64-1.121.04611.tar.gz",
-      "size": 182276606
+      "name": "Black.IDE-linux-loong64-1.121.04828.tar.gz",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-linux-loong64-1.121.04828.tar.gz",
+      "size": 181411080
     },
     {
-      "name": "Black.IDE-linux-armhf-1.121.04611.tar.gz",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-linux-armhf-1.121.04611.tar.gz",
-      "size": 171549025
+      "name": "Black.IDE-linux-ppc64le-1.121.04828.tar.gz",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-linux-ppc64le-1.121.04828.tar.gz",
+      "size": 208407074
     },
     {
-      "name": "Black.IDE-linux-loong64-1.121.04611.tar.gz",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-linux-loong64-1.121.04611.tar.gz",
-      "size": 181066191
+      "name": "Black.IDE-linux-riscv64-1.121.04828.tar.gz",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-linux-riscv64-1.121.04828.tar.gz",
+      "size": 181239315
     },
     {
-      "name": "Black.IDE-linux-ppc64le-1.121.04611.tar.gz",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-linux-ppc64le-1.121.04611.tar.gz",
-      "size": 208055580
+      "name": "Black.IDE-linux-x64-1.121.04828.tar.gz",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-linux-x64-1.121.04828.tar.gz",
+      "size": 186845219
     },
     {
-      "name": "Black.IDE-linux-riscv64-1.121.04611.tar.gz",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-linux-riscv64-1.121.04611.tar.gz",
-      "size": 180856281
+      "name": "Black.IDE-win32-arm64-1.121.04828.zip",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-win32-arm64-1.121.04828.zip",
+      "size": 200778255
     },
     {
-      "name": "Black.IDE-linux-x64-1.121.04611.tar.gz",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-linux-x64-1.121.04611.tar.gz",
-      "size": 186469276
+      "name": "Black.IDE-win32-x64-1.121.04828.zip",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDE-win32-x64-1.121.04828.zip",
+      "size": 204420648
     },
     {
-      "name": "Black.IDE-win32-arm64-1.121.04611.zip",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-win32-arm64-1.121.04611.zip",
-      "size": 200374698
+      "name": "Black.IDESetup-arm64-1.121.04828.exe",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDESetup-arm64-1.121.04828.exe",
+      "size": 139503226
     },
     {
-      "name": "Black.IDE-win32-x64-1.121.04611.zip",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE-win32-x64-1.121.04611.zip",
-      "size": 204030022
+      "name": "Black.IDESetup-x64-1.121.04828.exe",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDESetup-x64-1.121.04828.exe",
+      "size": 146233848
     },
     {
-      "name": "Black.IDE.arm64.1.121.0.dmg",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDE.arm64.1.121.0.dmg",
-      "size": 267558096
+      "name": "Black.IDEUserSetup-arm64-1.121.04828.exe",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDEUserSetup-arm64-1.121.04828.exe",
+      "size": 139503588
     },
     {
-      "name": "Black.IDESetup-arm64-1.121.04611.exe",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDESetup-arm64-1.121.04611.exe",
-      "size": 139329655
+      "name": "Black.IDEUserSetup-x64-1.121.04828.exe",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black.IDEUserSetup-x64-1.121.04828.exe",
+      "size": 146234115
     },
     {
-      "name": "Black.IDESetup-x64-1.121.04611.exe",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDESetup-x64-1.121.04611.exe",
-      "size": 146093389
+      "name": "Black_IDE-1.121.04828.glibc2.30-x86_64.AppImage",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black_IDE-1.121.04828.glibc2.30-x86_64.AppImage",
+      "size": 191427776
     },
     {
-      "name": "Black.IDEUserSetup-arm64-1.121.04611.exe",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDEUserSetup-arm64-1.121.04611.exe",
-      "size": 139329979
-    },
-    {
-      "name": "Black.IDEUserSetup-x64-1.121.04611.exe",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black.IDEUserSetup-x64-1.121.04611.exe",
-      "size": 146093750
-    },
-    {
-      "name": "Black_IDE-1.121.04611.glibc2.30-x86_64.AppImage",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black_IDE-1.121.04611.glibc2.30-x86_64.AppImage",
-      "size": 191096000
-    },
-    {
-      "name": "Black_IDE-1.121.04611.glibc2.30-x86_64.AppImage.zsync",
-      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04611/Black_IDE-1.121.04611.glibc2.30-x86_64.AppImage.zsync",
-      "size": 326851
+      "name": "Black_IDE-1.121.04828.glibc2.30-x86_64.AppImage.zsync",
+      "browser_download_url": "https://github.com/ornate-source/blackIDE/releases/download/1.121.04828/Black_IDE-1.121.04828.glibc2.30-x86_64.AppImage.zsync",
+      "size": 327418
     }
   ]
 };
